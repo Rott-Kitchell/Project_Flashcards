cb87ad096b8c959aa4751fc6f78ad018b88d6b08
RottK <r.kitchell815@gmail.com>
2021-03-21 20:08:09 -0500

    merged index and home

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,17 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Route } from "react-router";
 import Header from "./Header";
-import Home from "./Home";
+
+import { Switch } from "react-router-dom";
+import DeckList from "./decks/DeckList";
+import DecksNav from "./decks/DecksNav";
+import NotFound from "./NotFound";
 
 function Layout() {
+  const [decks, setDecks] = useState([]);
   return (
     <Fragment>
       <Header />
       <div className="container px-5">
         {/* TODO: Implement the screen starting here */}
-        <Route>
-          <Home />
-        </Route>
+        <Switch>
+          <Route path="/decks">
+            <DecksNav setDecks={setDecks} decks={decks} />
+          </Route>
+          <Route exact path="/">
+            <DeckList setDecks={setDecks} decks={decks} />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </Fragment>
   );
